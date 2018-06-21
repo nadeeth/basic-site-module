5a9b27105688da26acef04571c4adba9ec8b8a23
Nadeeth Lansakara <nadeeth.lansakara@solnet.co.nz>
2018-06-21 12:24:46 +1200

    SlideFullscreen object

---
+++ src/SlideFullscreen.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Site\DataObjects;
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
+class SlideFullscreen extends DataObject
+{
+    private static $db = [
+        'Title' => 'Varchar(50)',
+        'Text' => DBHTMLText::class
+    ];
+
+    private static $has_one = [
+        'Image' => Image::class,
+        'HomePageFullscreen' => 'HomePageFullscreen'
+    ];
+
+    private static $table_name = "SlideFullscreen";
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->addFieldToTab("Root.Main", HTMLEditorField::create('Text'));
+        $fields->addFieldToTab("Root.Main", UploadField::create('Image', 'Image (2000 x 1333)'));
+
+        return $fields;
+    }
+}

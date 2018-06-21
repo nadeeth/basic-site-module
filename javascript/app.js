4a5fda0f3f1ad4846f28a4b64a986914220c8155
Nadeeth Lansakara <nadeeth.lansakara@solnet.co.nz>
2018-06-22 10:49:31 +1200

    Home page carouse next/prev actions

--- javascript/app.js
+++ javascript/app.js
@@ -2,9 +2,17 @@
 
 $(function () {
 
+    //Main Menu Scroll
     $(document).scroll(function () {
         var $nav = $(".navbar.fixed-top");
         $nav.toggleClass('bg-dark', $(this).scrollTop() > $nav.height());
     });
-    
+
+    //FullScreenHomePage Carousell
+    $('.carousel-control-next').click(function() {
+        $("#carouselControls").carousel('next');
+    });
+    $('.carousel-control-prev').click(function() {
+        $("#carouselControls").carousel('prev');
+    });
 });
